2c6997855e3811538562262069946bdce66c2906
sailalithprasad <sailalithprasad.pathi@clairvoyantsoft.com>
2017-05-31 14:16:39 -0700

    Intial

---
+++ src/Chatclient.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+/**
+ * Created by Sai Lalith on 5/31/2017.
+ */
+public class Chatclient {
+    public void connect(){
+        try{
+            Socket s = new Socket("localhost",5000);
+            InputStreamReader inputStreamReader = new InputStreamReader(s.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String msg = bufferedReader.readLine();
+                System.out.println(msg);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void main(String args[]){
+        new Chatclient().connect();
+    }
+}
--- src/Chatserver.java
+++ src/Chatserver.java
@@ -1,5 +1,27 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by Sai Lalith on 5/31/2017.
  */
 public class Chatserver {
+    public void makeserver() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            while(true) {
+                Socket s = serverSocket.accept();
+                System.out.print("Connected");
+                PrintWriter printWriter = new PrintWriter(s.getOutputStream());
+                printWriter.print("Connected to the server at port"+s.getPort());
+                printWriter.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String ar[]){
+            new Chatserver().makeserver();
+    }
 }
