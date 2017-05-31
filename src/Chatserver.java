acbe3e8d0c8a33102303c55c1c8d9f71746ac44c
sailalithprasad <sailalithprasad.pathi@clairvoyantsoft.com>
2017-05-31 16:35:52 -0700

    Intial

--- src/Chatserver.java
+++ src/Chatserver.java
@@ -2,18 +2,21 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 /**
  * Created by Sai Lalith on 5/31/2017.
  */
 public class Chatserver {
+    ArrayList<Socket> connectedClients;
     public void makeserver() {
         try {
             ServerSocket serverSocket = new ServerSocket(5000);
             Scanner scanner = new Scanner(System.in);
             while(true) {
                 Socket s = serverSocket.accept();
-                System.out.print("Connected");
+                connectedClients.add(s);
+                System.out.print("Client Connected");
                 PrintWriter printWriter = new PrintWriter(s.getOutputStream());
                 printWriter.print("Connected to the server at port"+s.getPort());
                 printWriter.close();
