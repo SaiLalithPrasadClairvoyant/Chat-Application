ad99c39007afc1526a2a66a2fa6b603184247059
sailalithprasad <sailalithprasad.pathi@clairvoyantsoft.com>
2017-06-01 14:48:28 -0700

    Intial

--- src/Chatclient.java
+++ src/Chatclient.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 /**
  * Created by Sai Lalith on 5/31/2017.
@@ -8,15 +10,64 @@ public class Chatclient {
     public void connect(){
         try{
             Socket s = new Socket("localhost",5000);
-            InputStreamReader inputStreamReader = new InputStreamReader(s.getInputStream());
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            String msg = bufferedReader.readLine();
-            System.out.println(msg);
-            s.close();
-            bufferedReader.close();
+            ReadThread readThread = new ReadThread(s);
+            Thread t1 = new Thread(readThread);
+            t1.start();
+            WriteThread writeThread = new WriteThread(s);
+            Thread t2 = new Thread(writeThread);
+            t2.start();
         }
         catch (Exception e){
             e.printStackTrace();
+        }
+    }
+    public class ReadThread implements Runnable{
+        Socket s;
+        public ReadThread(Socket clientSocket){
+            this.s = clientSocket;
+        }
+        @Override
+        public void run() {
+            try {
+                InputStreamReader inputStreamReader = new InputStreamReader(s.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String msg;
+                while(true){
+                    while((msg = bufferedReader.readLine())!= null){
+                        if(msg.equals("Bye")){
+                            System.out.println("Server said BYE !!");
+                            System.exit(0);
+                        }else{
+                            System.out.println(msg);
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+    public class WriteThread implements Runnable{
+        Socket s;
+        public WriteThread(Socket clientSocket){
+            this.s = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                PrintWriter pw = new PrintWriter(s.getOutputStream());
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+                String msg;
+                while(true){
+                    msg = bufferedReader.readLine();
+                    pw.println(msg);
+                    pw.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
     public static void main(String args[]){
--- src/Chatserver.java
+++ src/Chatserver.java
@@ -1,25 +1,29 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Scanner;
+
 /**
  * Created by Sai Lalith on 5/31/2017.
  */
 public class Chatserver {
-    ArrayList<Socket> connectedClients;
+    public static ArrayList<Socket> clients = new ArrayList<>();
     public void makeserver() {
         try {
             ServerSocket serverSocket = new ServerSocket(5000);
-            Scanner scanner = new Scanner(System.in);
             while(true) {
                 Socket s = serverSocket.accept();
-                connectedClients.add(s);
-                System.out.print("Client Connected");
-                PrintWriter printWriter = new PrintWriter(s.getOutputStream());
-                printWriter.print("Connected to the server at port"+s.getPort());
-                printWriter.close();
+                System.out.print("Client Connected at port" + s.getPort());
+                //clients.add(s);
+                ReadThread readThread = new ReadThread(s);
+                Thread t1 = new Thread(readThread);
+                t1.start();
+                WriteThread writeThread = new WriteThread(s);
+                Thread t2 = new Thread(writeThread);
+                t2.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -28,4 +32,67 @@ public class Chatserver {
     public static void main(String ar[]){
             new Chatserver().makeserver();
     }
+    class ReadThread implements Runnable{
+        Socket s;
+        public ReadThread(Socket clientSocket){
+            this.s = clientSocket;
+        }
+        @Override
+        public void run() {
+            try {
+                InputStreamReader inputStreamReader = new InputStreamReader(s.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String msgFromClient;
+                while(true){
+                    while((msgFromClient = bufferedReader.readLine())!=null){
+                        if(msgFromClient.equals("Bye")){
+                            System.out.println("Client said BYE !!");
+                            System.exit(0);
+                        }else{
+                         System.out.println(msgFromClient);
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+    class WriteThread implements Runnable{
+        Socket s;
+        public WriteThread(Socket clientSocket){
+            this.s = clientSocket;
+        }
+        @Override
+        public void run() {
+            try {
+                PrintWriter pw = new PrintWriter(s.getOutputStream());
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+                String msg;
+                while(true){
+                    msg = bufferedReader.readLine();
+                    pw.println(msg);
+                    pw.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+   /* public class Maintain implements Runnable{
+        Socket s ;
+        public Maintain(Socket s){
+            this.s = s;
+        }
+        @Override
+        public void run() {
+            ReadThread readThread= new ReadThread(s);
+            Thread t1 = new Thread(readThread);
+            t1.start();
+            WriteThread writeThread = new WriteThread(s);
+            Thread t2 = new Thread(writeThread);
+            t2.start();
+        }
+    }*/
 }
